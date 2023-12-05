9ddd0129af65be6d64cdad4239639639fdb74be8
gk27min <gessicadesouza2022@gmail.com>
2023-12-05 12:02:04 -0300

    classifier final version

--- src/classifier.py
+++ src/classifier.py
@@ -3,6 +3,7 @@ import json
 import wisardpkg as wsd
 import pandas as pd
 import numpy as np
+import scipy.sparse as sp
 import scipy.sparse
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -16,14 +17,34 @@ import random
 
 random_state = 42
 labels_column = 'LABEL'
-centroid_cal = False
-most_dist = False
-most_close = False
+centroid_cal = True
+most_dist = True
+most_close = True
 fixed_train = []
 
 random.seed(random_state)
 
-def select_train_base(labels, matrix):
+def remove_documents_by_classes(class_list, csr_matrix, label_vector):
+    # Crie um conjunto das classes a serem removidas para verificar a pertinência
+    classes_to_remove = set(class_list)
+
+    # Inicialize listas vazias para armazenar os índices e rótulos a serem mantidos
+    indices_to_keep = []
+    labels_to_keep = []
+
+    # Itere pelos documentos e rótulos, mantendo apenas aqueles que não pertencem às classes a serem removidas
+    for i, label in enumerate(label_vector):
+        if label not in classes_to_remove:
+            indices_to_keep.append(i)
+            labels_to_keep.append(label)
+
+    # Crie uma nova matriz CSR e vetor de rótulos com base nos índices mantidos
+    csr_matrix_filtered = csr_matrix[indices_to_keep]
+    label_vector_filtered = np.array(labels_to_keep)
+
+    return csr_matrix_filtered, label_vector_filtered
+
+def select_train_base(labels, matrix, fixed_indices=None):
     indices_train = []
     indices_test = []
     labels_unique, counts = np.unique(labels, return_counts=True)
@@ -31,21 +52,25 @@ def select_train_base(labels, matrix):
     for label, num_docs in zip(labels_unique, counts):
         # Define a porcentagem de treino baseada no número de documentos
         if num_docs < 300:
-            percent_train = 0.70
+            percent_train = 0.90
         elif num_docs < 500:
+            percent_train = 0.80
+        elif num_docs < 1500:
             percent_train = 0.70
-        elif num_docs < 1000:
-            percent_train = 0.85
         elif num_docs < 2000:
-            percent_train = 0.75
-        else:
             percent_train = 0.60
+        else:
+            percent_train = 0.50
         
         # Encontra os índices para a classe atual
         class_indices = np.where(labels == label)[0]
 
         # Divide os índices para treino e teste
         train_indices, test_indices = train_test_split(class_indices, train_size=percent_train)
+        
+        if fixed_indices is not None:
+            # Adicione os índices fixos à lista de índices de treinamento
+            train_indices = list(set(train_indices) | set(fixed_indices))
         
         indices_train.extend(train_indices)
         indices_test.extend(test_indices)
@@ -56,35 +81,36 @@ def select_train_base(labels, matrix):
     y_train = [labels[i] for i in indices_train]
     y_test = [labels[i] for i in indices_test]
 
-
     return X_train, X_test, y_train, y_test
 
-def centroid_calc():
+def centroid_calc(vectorized_):
     centroids = {}
     for label in set(labels_uniq):
         # Índices dos documentos que pertencem a esta classe
         indices = [i for i, l in enumerate(labels) if l == label]
         
         # Calculando a média dos vetores TF-IDF para esses documentos
-        centroids[label] = np.mean(vectorized[indices].toarray(), axis=0)
-        valores = list(centroids.values())
-        matriz = np.array(valores)
-    return csr_matrix(matriz), list(centroids.keys())
+        centroids[label] = np.mean(vectorized_[indices], axis=0)
+    
+    # Converta os valores do dicionário para uma matriz
+    centroid_matrix = np.array(list(centroids.values()))
+    
+    return csr_matrix(centroid_matrix), list(centroids.keys())
 
-def most_distants_docs():
+def most_distants_docs(vectorized_):
     most_distant_indices = []
 
     for label in set(labels):
         indices = [i for i, l in enumerate(labels) if l == label]
 
         # Calcular as distâncias de cosseno entre os documentos
-        distances = cosine_distances(vectorized[indices])
+        distances = cosine_distances(vectorized_[indices])
 
         # Binarizar as distâncias (manter apenas 0s e 1s)
         binary_distances = np.where(distances > 0, 1, 0)
 
-        # Encontrar os índices dos documentos mais distantes (20%)
-        num_docs_to_select = int(binary_distances.shape[1] * 0.5)
+        # Encontrar os índices dos documentos mais distantes (10)
+        num_docs_to_select = 10
         selected_indices = np.argsort(binary_distances.sum(axis=1))[-num_docs_to_select:]
 
         # Mapear os índices selecionados de volta para os índices originais
@@ -95,20 +121,20 @@ def most_distants_docs():
 
     return most_distant_indices
 
-def most_close_docs():
+def most_close_docs(vectorized_):
     most_close_indices = []
 
     for label in set(labels):
         indices = [i for i, l in enumerate(labels) if l == label]
 
         # Calcular as distâncias de cosseno entre os documentos
-        distances = cosine_distances(vectorized[indices])
+        distances = cosine_distances(vectorized_[indices])
 
         # Binarizar as distâncias (manter apenas 0s e 1s)
         binary_distances = np.where(distances > 0, 1, 0)
 
-        # Encontrar os índices dos documentos mais próximos (20%)
-        num_docs_to_select = int(binary_distances.shape[1] * 0.2)
+        # Encontrar os índices dos documentos mais próximos (10)
+        num_docs_to_select = 10
         selected_indices = np.argsort(binary_distances.sum(axis=1))[:num_docs_to_select]
 
         # Mapear os índices selecionados de volta para os índices originais
@@ -125,7 +151,7 @@ def binarize_vectorized(thermometer, X):
     thermometer_sizes = [thermometer] * num_features
 
     # Converter para formato denso se X for uma matriz esparsa
-    if isinstance(X, scipy.sparse.spmatrix):
+    if sp.issparse(X):
         X = X.toarray()
 
     mins = np.min(X, axis=0)
@@ -258,22 +284,9 @@ print('Getting all inicial files and selecting all features...')
 
 params = {'thermometer': 62,'ram': 62}
 
-if centroid_cal:
-    centroids, labels_centroids = centroid_calc()
-    centroids_bin = binarize_vectorized(params['thermometer'], centroids)
-    vectorized.extend(centroids_bin)
-    labels.extend(labels_centroids)
-    print('Centroids calculate')
-
-if most_dist:
-    indices = most_distants_docs()
-    fixed_train.extend(indices)
-    print('More distantes calculate')
-
-if most_close:
-    indices = most_close_docs()
-    fixed_train.extend(indices)
-    print('Centroid, most distants, most close documents calculate...')
+# vectorized, labels = remove_documents_by_classes(['imo', 'mic', 'mul', 'tav'], vectorized, labels)
+# labels_uniq = list(set(labels))
+# labels_uniq.sort()
 
 n_components = 100  # Ajuste conforme necessário
 
@@ -294,15 +307,36 @@ plt.savefig('/home/gssilva/datasets/atribuna-elias/full/results/imagens/svd100.p
 plt.close()
 print('Aply svd model on the data and save svd curve of \'Explained Variance Ratio of SVD\' ...')
 
+if centroid_cal:
+    centroids, labels_centroids = centroid_calc(vectorized_reduced)
+    centroids_bin = binarize_vectorized(params['thermometer'], centroids)
+    print('Centroids calculate....')
+
+if most_dist:
+    indices = most_distants_docs(vectorized_reduced)
+    fixed_train.extend(indices)
+    print('Most distantes calculate....')
+
+if most_close:
+    indices = most_close_docs(vectorized_reduced)
+    fixed_train.extend(indices)
+    print('Most close documents calculate...')
+
 bin_x = binarize_vectorized(params['thermometer'], vectorized_reduced)
-bin_train, bin_test, y_train, y_test = select_train_base(labels, bin_x)
+bin_train, bin_test, y_train, y_test = select_train_base(labels, bin_x, fixed_train)
+
+if centroid_cal:
+    bin_train.extend(centroids_bin)
+    y_train = list(y_train)
+    y_train.extend(labels_centroids)
+
 print('binarized and split all data done...')
 
 ds_train = wsd.DataSet(bin_train, y_train)
 ds_test = wsd.DataSet(bin_test, y_test)
 print('Dataset Wisard Concluide...')
 
-model = wsd.ClusWisard(params['ram'], 0.2, 100, 5)
+model = wsd.ClusWisard(params['ram'], 0.5, 1000, 5)
 model.train(ds_train)
 print('Training model concluide...')
 
